1c0807bc0486e03b5843140c277bcabd145d33ff
Yuvraj Chettri <chettriyuvraj@gmail.com>
2024-03-27 23:30:17 +0530

    1. refactor: simplify test suite with helper functions

--- db/db_test.go
+++ db/db_test.go
@@ -68,15 +68,9 @@ func TestLevelDBIteratorNext(t *testing.T) {
 	iterations := 10
 
 	/* Test Empty Iterator */
-	exists, err := iterator.Next(), iterator.Error()
-	require.NoError(t, err)
-	require.False(t, exists)
-	keyGot, err := iterator.Key(), iterator.Error()
-	require.NoError(t, err)
-	require.Equal(t, []byte{}, keyGot)
-	valGot, err := iterator.Value(), iterator.Error()
-	require.NoError(t, err)
-	require.Equal(t, []byte{}, valGot)
+	iteratorTestNext(t, iterator, false, false)
+	iteratorTestKey(t, iterator, []byte{}, false)
+	iteratorTestVal(t, iterator, []byte{}, false)
 
 	/* Populate db */
 	for i := 0; i < iterations; i++ {
@@ -88,32 +82,19 @@ func TestLevelDBIteratorNext(t *testing.T) {
 	/* Move iterator over all values and check that they exist + are correct + no errors */
 	for i := 0; i < iterations; i++ {
 		keyExpected, valExpected := []byte(fmt.Sprintf("key%d", i)), []byte(fmt.Sprintf("val%d", i))
-		keyGot, err := iterator.Key(), iterator.Error()
-		require.NoError(t, err)
-		require.Equal(t, keyExpected, keyGot)
-		valGot, err := iterator.Value(), iterator.Error()
-		require.NoError(t, err)
-		require.Equal(t, valExpected, valGot)
-
-		nextExists, err := iterator.Next(), iterator.Error()
-		require.NoError(t, err)
+		iteratorTestKey(t, iterator, keyExpected, false)
+		iteratorTestVal(t, iterator, valExpected, false)
 		if i < iterations-1 {
-			require.True(t, nextExists)
+			iteratorTestNext(t, iterator, true, false)
 		} else {
-			require.False(t, nextExists)
+			iteratorTestNext(t, iterator, false, false)
 		}
 	}
 
 	/* After all values are exhausted 1st two checks already tested in last iteration of loop, just keeping both checks to be consistent */
-	exists, err = iterator.Next(), iterator.Error()
-	require.NoError(t, err)
-	require.False(t, exists)
-	keyGot, err = iterator.Key(), iterator.Error()
-	require.NoError(t, err)
-	require.Equal(t, []byte{}, keyGot)
-	valGot, err = iterator.Value(), iterator.Error()
-	require.NoError(t, err)
-	require.Equal(t, []byte{}, valGot)
+	iteratorTestNext(t, iterator, false, false)
+	iteratorTestKey(t, iterator, []byte{}, false)
+	iteratorTestVal(t, iterator, []byte{}, false)
 }
 
 func TestRangeScan(t *testing.T) {
@@ -125,67 +106,74 @@ func TestRangeScan(t *testing.T) {
 		require.NoError(t, err)
 	}
 
-	/* Check exact ranges - also confirm once values exhausted */
+	/* Check exact ranges + confirm if values exhausted afterwards */
 	start, end := []byte("key1"), []byte("key9")
 	iterator, err := db.RangeScan(start, end)
 	require.NoError(t, err)
 	for i := 1; i <= 9; i += 2 {
 		keyExpected, valExpected := []byte(fmt.Sprintf("key%d", i)), []byte(fmt.Sprintf("val%d", i))
-		keyGot, err := iterator.Key(), iterator.Error()
-		require.NoError(t, err)
-		require.Equal(t, keyExpected, keyGot)
-		valGot, err := iterator.Value(), iterator.Error()
-		require.NoError(t, err)
-		require.Equal(t, valExpected, valGot)
-
-		nextExists, err := iterator.Next(), iterator.Error()
-		require.NoError(t, err)
-		if i < 9 {
-			require.True(t, nextExists)
+		iteratorTestKey(t, iterator, keyExpected, false)
+		iteratorTestVal(t, iterator, valExpected, false)
+		if i < iterations {
+			iteratorTestNext(t, iterator, true, false)
 		} else {
-			require.False(t, nextExists)
+			iteratorTestNext(t, iterator, false, false)
 		}
 	}
+	iteratorTestNext(t, iterator, false, false)
+	iteratorTestKey(t, iterator, []byte{}, false)
+	iteratorTestVal(t, iterator, []byte{}, false)
 
-	exists, err := iterator.Next(), iterator.Error()
-	require.NoError(t, err)
-	require.False(t, exists)
-	keyGot, err := iterator.Key(), iterator.Error()
-	require.NoError(t, err)
-	require.Equal(t, []byte{}, keyGot)
-	valGot, err := iterator.Value(), iterator.Error()
-	require.NoError(t, err)
-	require.Equal(t, []byte{}, valGot)
-
-	/* Check inexact ranges - also confirm once values exhausted */
+	/* Check inexact ranges + confirm if values exhausted afterwards */
 	start, end = []byte("key"), []byte("key8")
 	iterator, err = db.RangeScan(start, end)
 	require.NoError(t, err)
 	for i := 1; i <= 7; i += 2 {
 		keyExpected, valExpected := []byte(fmt.Sprintf("key%d", i)), []byte(fmt.Sprintf("val%d", i))
-		keyGot, err := iterator.Key(), iterator.Error()
-		require.NoError(t, err)
-		require.Equal(t, keyExpected, keyGot)
-		valGot, err := iterator.Value(), iterator.Error()
-		require.NoError(t, err)
-		require.Equal(t, valExpected, valGot)
-
-		nextExists, err := iterator.Next(), iterator.Error()
-		require.NoError(t, err)
+		iteratorTestKey(t, iterator, keyExpected, false)
+		iteratorTestVal(t, iterator, valExpected, false)
 		if i < 7 {
-			require.True(t, nextExists)
+			iteratorTestNext(t, iterator, true, false)
 		} else {
-			require.False(t, nextExists)
+			iteratorTestNext(t, iterator, false, false)
 		}
 	}
+	iteratorTestNext(t, iterator, false, false)
+	iteratorTestKey(t, iterator, []byte{}, false)
+	iteratorTestVal(t, iterator, []byte{}, false)
+}
 
-	exists, err = iterator.Next(), iterator.Error()
-	require.NoError(t, err)
-	require.False(t, exists)
-	keyGot, err = iterator.Key(), iterator.Error()
-	require.NoError(t, err)
-	require.Equal(t, []byte{}, keyGot)
-	valGot, err = iterator.Value(), iterator.Error()
-	require.NoError(t, err)
-	require.Equal(t, []byte{}, valGot)
+/* NOTE: This will potentially modify the iterator by calling Next() */
+func iteratorTestNext(t *testing.T, iterator Iterator, existsWant bool, errWant bool) {
+	t.Helper()
+	exists, err := iterator.Next(), iterator.Error()
+	require.Equal(t, existsWant, exists)
+	if errWant {
+		require.Error(t, err)
+	} else {
+		require.NoError(t, err)
+	}
+}
+
+func iteratorTestKey(t *testing.T, iterator Iterator, keyWant []byte, errWant bool) {
+	t.Helper()
+	keyGot, err := iterator.Key(), iterator.Error()
+	require.Equal(t, keyWant, keyGot)
+	if errWant {
+		require.Error(t, err)
+	} else {
+		require.NoError(t, err)
+	}
+}
+
+func iteratorTestVal(t *testing.T, iterator Iterator, valWant []byte, errWant bool) {
+	t.Helper()
+	t.Helper()
+	valGot, err := iterator.Value(), iterator.Error()
+	require.Equal(t, valWant, valGot)
+	if errWant {
+		require.Error(t, err)
+	} else {
+		require.NoError(t, err)
+	}
 }
