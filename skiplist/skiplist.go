7c22ca2737d2f3a1379ab9ba343673ccf4ba9348
Yuvraj Chettri <chettriyuvraj@gmail.com>
2024-03-29 02:53:39 +0530

    feat: implement insert and delete for skip lists

---
+++ skiplist/skiplist.go
@@ -0,0 +1,149 @@
+package skiplist
+
+import (
+	"bytes"
+	"errors"
+	"fmt"
+	"math/rand"
+	"strings"
+)
+
+var ErrInvalidNodeLevel = errors.New("invalid node level")
+
+type Node struct {
+	key     []byte
+	forward []*Node
+}
+
+type SkipList struct {
+	head     *Node
+	level    int
+	nil      *Node
+	p        float64
+	maxLevel int
+}
+
+func NewSkipList(p float64, maxLevel int) *SkipList {
+	skiplist := SkipList{head: NewNode([]byte{}), nil: NewNode([]byte{}), level: 1, p: p, maxLevel: maxLevel}
+	skiplist.head.forward = append(skiplist.head.forward, skiplist.nil)
+	return &skiplist
+}
+
+func (sl *SkipList) String() string {
+	var sb strings.Builder
+	sb.WriteString("Head: ")
+	sb.WriteString(sl.head.String())
+	for curNode := sl.head.forward[1]; curNode != sl.nil; curNode = curNode.forward[1] {
+		sb.WriteString(curNode.String())
+	}
+	return sb.String()
+}
+
+func (node *Node) String() string {
+	var sb strings.Builder
+	sb.WriteString(fmt.Sprintf("\n\nKey: %s", string(node.key)))
+	for i := 1; i <= node.getLevel(); i++ {
+		sb.WriteString(fmt.Sprintf("\nLevel %d: %s\n", i, string(node.forward[i].key)))
+	}
+	return sb.String()
+}
+
+/*
+- Initialize new node using this function ONLY
+- Returns new node after appending a dummy level to 0th index
+*/
+func NewNode(key []byte) *Node {
+	node := &Node{key: key}
+	node.forward = append(node.forward, &Node{})
+	return node
+}
+
+/*
+- Assuming first level is always a dummy level
+*/
+func (node *Node) getLevel() int {
+	return len(node.forward) - 1
+}
+
+func (sl *SkipList) Search(key []byte) *Node {
+	/* First search for the insertion spot using a dummy search node with the same key*/
+	node, searchNode := sl.head, NewNode(key)
+	for i := sl.level; i > 0; i-- {
+		for sl.compare(node.forward[i], searchNode) < 0 { /* As long as node's key is less than forward[i] */
+			node = node.forward[i]
+		}
+	}
+
+	nodeAhead := node.forward[1]
+	if nodeAhead != nil && sl.compare(node, nodeAhead) == 0 {
+		return node
+	}
+	return nil
+}
+
+func (sl *SkipList) Insert(key []byte) error {
+	/* Init */
+	node, newNode := sl.head, NewNode(key)
+	updateList := []*Node{NewNode([]byte{})}
+	for i := 1; i <= sl.maxLevel; i++ {
+		updateList = append(updateList, NewNode([]byte{}))
+	}
+
+	/* First search for the insertion spot */
+	for i := sl.level; i > 0; i-- {
+		for sl.compare(node.forward[i], newNode) < 0 { /* As long as node.forward[i] is less than search key */
+			node = node.forward[i]
+		}
+		updateList[i] = node
+	}
+
+	/* If key already exists, simply update value */
+	nodeAhead := node.forward[1]
+	if nodeAhead != nil && sl.compare(node, nodeAhead) == 0 {
+		nodeAhead.key = key
+		return nil
+	}
+
+	/* Else append new node with random level */
+	randomLevel := sl.randomLevel()
+	if randomLevel > sl.level {
+		for i := sl.level + 1; i <= randomLevel; i++ {
+			updateList[i] = sl.head
+			sl.head.forward = append(sl.head.forward, sl.nil)
+		}
+		sl.level = randomLevel
+	}
+	for i := 1; i <= sl.level; i++ {
+		newNode.forward = append(newNode.forward, updateList[i].forward[i])
+		updateList[i].forward[i] = newNode
+	}
+
+	return nil
+}
+
+/*
+- Handles comparison for 'nil' element in skiplist gracefully - use this for comparison ops in search/insert/delete
+*/
+func (sl *SkipList) compare(n1, n2 *Node) int {
+	if n1 == n2 {
+		return 0
+	}
+
+	if n1 == sl.nil {
+		return 1
+	}
+
+	if n2 == sl.nil {
+		return -1
+	}
+
+	return bytes.Compare(n1.key, n2.key)
+}
+
+func (sl *SkipList) randomLevel() int {
+	lvl := 1
+	for rand.Float64() < sl.p && lvl < sl.maxLevel {
+		lvl++
+	}
+	return lvl
+}
