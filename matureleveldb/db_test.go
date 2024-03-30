f938446d2966c43255c30374acc22b1f979bacf6
Yuvraj Chettri <chettriyuvraj@gmail.com>
2024-03-30 18:35:43 +0530

    1. test: add tests using common test suite

--- matureleveldb/db_test.go
+++ matureleveldb/db_test.go
@@ -1,13 +1,27 @@
 package matureleveldb
 
 import (
-	"fmt"
 	"testing"
 
+	"github.com/chettriyuvraj/leveldb-clone/common"
 	"github.com/chettriyuvraj/leveldb-clone/skiplist"
+	"github.com/chettriyuvraj/leveldb-clone/test"
 )
 
-func TestPackageImport(t *testing.T) {
-	sl := skiplist.NewSkipList(0.3, 1)
-	fmt.Println(*sl)
+/* Workaround done exclusively to match signature with test suite */
+func newLevelDBAsInterface() common.DB {
+	return &LevelDB{*skiplist.NewSkipList(P, MAXLEVEL)}
+}
+
+func newLevelDBIteratorAsInterface(db common.DB) common.Iterator {
+	return &LevelDBIterator{LevelDB: db.(*LevelDB)}
+}
+
+func TestDB(t *testing.T) {
+	test.TestDB(t, test.DBTester{New: newLevelDBAsInterface})
+	// test.TestIterator(t, test.IteratorTester{New: newLevelDBIteratorAsInterface}, test.DBTester{New: newLevelDBAsInterface})
+}
+
+func BenchmarkDB(b *testing.B) {
+	test.BenchmarkDB(b, test.DBTester{New: newLevelDBAsInterface})
 }
