2c8205b7d6fadae50a826fdbe9e2de9f94f6f0a0
Yuvraj Chettri <chettriyuvraj@gmail.com>
2024-04-01 15:03:53 +0530

    feat: add sample code to sample what skip lists look like

--- sample-usage/sample.go
+++ sample-usage/sample.go
@@ -1,31 +1,42 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/syndtr/goleveldb/leveldb"
-)
-
 func main() {
 
-	db, err := leveldb.OpenFile("./db/", nil)
-	defer db.Close()
-	if err != nil {
-		fmt.Println(err)
-	}
+	/* Check what files are created */
+	// testDBWithSomeData := func() {
+	// 	db, err := leveldb.OpenFile("./db/", nil)
+	// 	defer db.Close()
+	// 	if err != nil {
+	// 		fmt.Println(err)
+	// 	}
 
-	err = db.Put([]byte("bested2"), []byte("valuebested"), nil)
-	if err != nil {
-		fmt.Println(err)
-	}
+	// 	err = db.Put([]byte("bested2"), []byte("valuebested"), nil)
+	// 	if err != nil {
+	// 		fmt.Println(err)
+	// 	}
 
-	err = db.Put([]byte("casted2"), []byte("valuecasted"), nil)
-	if err != nil {
-		fmt.Println(err)
-	}
+	// 	err = db.Put([]byte("casted2"), []byte("valuecasted"), nil)
+	// 	if err != nil {
+	// 		fmt.Println(err)
+	// 	}
 
-	err = db.Put([]byte("aested2"), []byte("valueaested"), nil)
-	if err != nil {
-		fmt.Println(err)
-	}
+	// 	err = db.Put([]byte("aested2"), []byte("valueaested"), nil)
+	// 	if err != nil {
+	// 		fmt.Println(err)
+	// 	}
+	// }
+
+	/* Sample of what my skiplist looks like */
+	// displaySkipListSample := func() {
+	// 	size := 20
+	// 	sl := skiplist.NewSkipList(0.25, 12)
+	// 	for i := 0; i < size; i++ {
+	// 		randItem := rand.Intn(size)
+	// 		k, v := []byte(fmt.Sprintf("%d", randItem)), []byte(fmt.Sprintf("%d", randItem))
+	// 		sl.Insert(k, v)
+	// 	}
+	// 	fmt.Println(sl)
+	// }
+	// displaySkipListSample()
+
 }
