1456463f70a367db714a01b0e8737e2f0a79892d
Yuvraj Chettri <chettriyuvraj@gmail.com>
2024-04-09 03:45:35 +0530

    1. feat: implement range scan for sstables 2. test: implement tests for sstables

--- memdb/sstabledb.go
+++ memdb/sstabledb.go
@@ -17,6 +17,15 @@ type SSTableDB struct {
 	dir *SSTableDirectory
 }
 
+/* Iterator will store all the offsets for the range, and grab each one on each 'next' */
+type SSTableIterator struct {
+	db      *SSTableDB
+	entries []*SSTableDirEntry
+	idx     int
+	curVal  []byte /* After first call to Value(), this is cached */
+	err     error
+}
+
 var ErrNoSSTableDirOffset = errors.New("no offset for directory in SSTable file")
 var ErrInvalidSSTableDirOffset = errors.New("dir offset does not exist in SSTable file")
 
@@ -48,7 +57,7 @@ func getSSTableDir(SSTableData []byte) (*SSTableDirectory, error) {
 
 	dirOffset := binary.BigEndian.Uint64(SSTableData[:8])
 	curOffset := dirOffset
-	dir := SSTableDirectory{entries: []SSTableDirEntry{}}
+	dir := SSTableDirectory{entries: []*SSTableDirEntry{}}
 	for {
 		if curOffset+4 > uint64(len(SSTableData)) {
 			break
@@ -69,7 +78,7 @@ func getSSTableDir(SSTableData []byte) (*SSTableDirectory, error) {
 		curOffset += 8
 
 		dirEntry := SSTableDirEntry{len: keyLen, key: key, offset: keyOffset}
-		dir.entries = append(dir.entries, dirEntry)
+		dir.entries = append(dir.entries, &dirEntry)
 	}
 
 	return &dir, nil
@@ -116,6 +125,14 @@ func (db *SSTableDB) Has(key []byte) (ret bool, err error) {
 	return true, nil
 }
 
+func (db *SSTableDB) RangeScan(start, limit []byte) (common.Iterator, error) {
+	iter, err := NewSSTableIterator(db, start, limit)
+	if err != nil {
+		return nil, err
+	}
+	return iter, nil
+}
+
 func (db *SSTableDB) Seek(offset int64, whence int) (int64, error) {
 	originOffset, err := db.f.Seek(offset, whence)
 	if err != nil {
@@ -126,4 +143,94 @@ func (db *SSTableDB) Seek(offset int64, whence int) (int64, error) {
 
 func (db *SSTableDB) Close() error {
 	return db.f.Close()
+}
+
+func NewSSTableIterator(db *SSTableDB, start, limit []byte) (*SSTableIterator, error) {
+	iter := &SSTableIterator{db: db}
+	entries, entriesN := db.dir.entries, len(db.dir.entries)
+
+	if bytes.Compare(start, limit) > 0 {
+		return iter, common.ErrInvalidRange
+	}
+
+	/* Find 2 indexes between which the range occurs */
+	startIdx := sort.Search(entriesN, func(i int) bool {
+		return bytes.Compare(entries[i].key, start) >= 0
+	})
+	if startIdx == entriesN {
+		return iter, nil
+	}
+
+	endIdx := sort.Search(entriesN, func(i int) bool {
+		return bytes.Compare(entries[i].key, limit) >= 0
+	})
+
+	/* Depending on the value and the key at endIdx, grab the entries of the range */
+	if endIdx == entriesN {
+		iter.entries = entries[startIdx:]
+		return iter, nil
+	}
+	if bytes.Equal(entries[endIdx].key, limit) {
+		iter.entries = entries[startIdx : endIdx+1]
+		return iter, nil
+	}
+
+	iter.entries = entries[startIdx:endIdx]
+	return iter, nil
+}
+
+func (iter *SSTableIterator) Next() bool {
+	iter.curVal = nil
+	iter.err = nil
+
+	if iter.idx == len(iter.entries)-1 {
+		iter.idx += 1
+		return false
+	}
+
+	if iter.idx >= len(iter.entries) {
+		return false
+	}
+
+	iter.idx += 1
+	return true
+}
+
+func (iter *SSTableIterator) Key() []byte {
+	iter.err = nil
+
+	if iter.idx >= len(iter.entries) {
+		return nil
+	}
+	return iter.entries[iter.idx].key
+}
+
+/*
+- We use Get which searches for the offset again, even though we already have the offset with us, can be improved
+*/
+func (iter *SSTableIterator) Value() []byte {
+	iter.err = nil
+	iter.curVal = nil
+
+	if iter.idx >= len(iter.entries) {
+		return nil
+	}
+
+	/* Check if already cached by a previous call to Value() */
+	if iter.curVal != nil {
+		return iter.curVal
+	}
+
+	key := iter.entries[iter.idx].key
+	val, err := iter.db.Get(key)
+	if err != nil {
+		iter.err = err
+	}
+	iter.curVal = val
+
+	return val
+}
+
+func (iter *SSTableIterator) Error() error {
+	return iter.err
 }
--- memdb/sstabledb_test.go
+++ memdb/sstabledb_test.go
@@ -2,10 +2,12 @@ package memdb
 
 import (
 	"bytes"
+	"fmt"
 	"io"
 	"testing"
 
 	"github.com/chettriyuvraj/leveldb-clone/common"
+	"github.com/chettriyuvraj/leveldb-clone/test"
 	"github.com/stretchr/testify/require"
 )
 
@@ -14,10 +16,10 @@ type BytesReadWriteSeekCloser struct {
 	*bytes.Reader
 }
 
-func (b BytesReadWriteSeekCloser) Write(p []byte) (n int, err error) {
+func (b *BytesReadWriteSeekCloser) Write(p []byte) (n int, err error) {
 	curData, err := io.ReadAll(b)
 	if err != nil {
-		return -1, err
+		return 0, err
 	}
 
 	updatedData := append(curData, p...)
@@ -60,4 +62,62 @@ func TestSSTableGet(t *testing.T) {
 	/* Check for non-existent record */
 	_, err := db.Get([]byte("randomVal"))
 	require.ErrorIs(t, err, common.ErrKeyDoesNotExist)
+}
+
+func TestSSTableRangeScan(t *testing.T) {
+	memdb, err := NewMemDB()
+	require.NoError(t, err)
+
+	/* Populate db */
+	for i := 1; i <= 9; i += 2 {
+		k, v := []byte(fmt.Sprintf("key%d", i)), []byte(fmt.Sprintf("val%d", i))
+		err := memdb.Put(k, v)
+		require.NoError(t, err)
+	}
+
+	/* Flush to an SSTable + get data as bytes + create in-mem sstable */
+	b := BytesReadWriteSeekCloser{bytes.NewReader([]byte{})}
+	err = memdb.flushSSTable(&b)
+	require.NoError(t, err)
+	sstData, err := io.ReadAll(b)
+	require.NoError(t, err)
+	dir, err := getSSTableDir(sstData)
+	require.NoError(t, err)
+	sstdb := SSTableDB{f: b, dir: dir}
+
+	/* Check exact ranges + confirm if values exhausted afterwards */
+	start, end := []byte("key1"), []byte("key9")
+	iterator, err := sstdb.RangeScan(start, end)
+	require.NoError(t, err)
+	for i := 1; i <= 9; i += 2 {
+		keyExpected, valExpected := []byte(fmt.Sprintf("key%d", i)), []byte(fmt.Sprintf("val%d", i))
+		test.IteratorTestKey(t, iterator, keyExpected, false)
+		test.IteratorTestVal(t, iterator, valExpected, false)
+		if i < 9 {
+			test.IteratorTestNext(t, iterator, true, false)
+		} else {
+			test.IteratorTestNext(t, iterator, false, false)
+		}
+	}
+	test.IteratorTestNext(t, iterator, false, false)
+	test.IteratorTestKey(t, iterator, nil, false)
+	test.IteratorTestVal(t, iterator, nil, false)
+
+	/* Check inexact ranges + confirm if values exhausted afterwards */
+	start, end = []byte("key"), []byte("key8")
+	iterator, err = sstdb.RangeScan(start, end)
+	require.NoError(t, err)
+	for i := 1; i <= 7; i += 2 {
+		keyExpected, valExpected := []byte(fmt.Sprintf("key%d", i)), []byte(fmt.Sprintf("val%d", i))
+		test.IteratorTestKey(t, iterator, keyExpected, false)
+		test.IteratorTestVal(t, iterator, valExpected, false)
+		if i < 7 {
+			test.IteratorTestNext(t, iterator, true, false)
+		} else {
+			test.IteratorTestNext(t, iterator, false, false)
+		}
+	}
+	test.IteratorTestNext(t, iterator, false, false)
+	test.IteratorTestKey(t, iterator, nil, false)
+	test.IteratorTestVal(t, iterator, nil, false)
 }
