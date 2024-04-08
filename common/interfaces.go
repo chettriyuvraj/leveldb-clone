a255f27e13688b1c78938e3db2536e9f45d017ad
Yuvraj Chettri <chettriyuvraj@gmail.com>
2024-04-08 15:33:11 +0530

    feat: add immutable db interface

--- common/interfaces.go
+++ common/interfaces.go
@@ -36,3 +36,16 @@ type DB interface {
 	// key-value pairs in the given range, ordered by key ascending.
 	RangeScan(start, limit []byte) (Iterator, error)
 }
+
+type ImmutableDB interface {
+	// Get gets the value for the given key. It returns an error if the
+	// DB does not contain the key.
+	Get(key []byte) (value []byte, err error)
+
+	// Has returns true if the DB contains the given key.
+	Has(key []byte) (ret bool, err error)
+
+	// RangeScan returns an Iterator (see below) for scanning through all
+	// key-value pairs in the given range, ordered by key ascending.
+	RangeScan(start, limit []byte) (Iterator, error)
+}
