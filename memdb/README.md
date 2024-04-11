771883e07fd578bb21f7e9282685bde0f2f239be
Yuvraj Chettri <chettriyuvraj@gmail.com>
2024-04-11 23:43:37 +0530

    refactor: decouple memdb from sstables

--- memdb/README.md
+++ memdb/README.md
@@ -1,61 +1,17 @@
 # README
 
-Two main things that this package comprises of: memdb and sstables
 
 **MemDB**: In-memory database that contains data upto a certain threshold.
-
-**SSTables**: MemDB can be flushed to disk once it exceeds a certain capacity, this representation is the SSTable (Sorted-String table)
-
-The high-level idea is that if you want to find an element, you would search the MemDB first, then the most recent SSTable, then the second most recent, and so on...
 
 ## MemDB
 
 - The idea in general is referred to as memtables (refer to DDIA Chapter 3: Storage and Retrieval)
 - The underlying Get, Put, Delete operations are implemented using a skip list
 
-## SSTables
-
-### Format
-
-- Directory Offset: 8 bytes to indicate the offset for the _Key Directory_
-- The data in the form of records, each individual record comprises of
-    - Key-length: 4 bytes
-    - Key: (key-length) bytes 
-    - Val-length: 4 bytes 
-    - Val: (val-length) bytes
-- Key Directory: contains data in the form of directory records, each individual record comprises of
-    - Key-length: 4 bytes
-    - Key: (key-length) bytes
-    - Offset: 8 bytes
-- Note: Our key directory does not contain all SSTables, but instead keys separated by a certain (gap) e.g 10 bytes, this is what is meant by a _sparse index_
-- Thus, our SSTable file looks like
-```
-[Dir Offset]
-[KV record 1]
-[KV record 2]
-    .
-    . 
-[Directory record 1]
-[Directory record 2]
-    .
-    .
-```
-
-### Reading SSTables
-- The basic idea is that SSTables are sorted and can be read using the key directory.
-- Steps to read a key: 
-    - Read the key directory into memory
-    - Perform a binary search in the struct for your key, find the closest key to the _left_ of your key i.e. greatest key that is smaller than or equal to your key
-    - Seek to that particular offset in the file and scan forwards until you find your key
-
-
 ## Misc
 - All our iterators are designed such that it contains a starting value before the first Next() call
-- I had initially created an SSTable where the directory contained every key, this took quite a while to change into our _sparse index_
 - An extra search op done to change db size for both get and put - to preserve simplicity instead of changing interface
 
 ### To dos
-- Tests for SSTables are practically unreadable + don't cover a lot of edges (I feel), refactor them.
-- Code for sstables get/iterating also feels like it can be refactored.
-- Also look at memdb implementation for refactors.
+- Look at memdb implementation for refactors.
 - DB Size computations not tested
--- memdb/memdb.go
+++ memdb/memdb.go
@@ -2,13 +2,13 @@ package memdb
 
 import (
 	"bytes"
-	"encoding/binary"
 	"errors"
 	"fmt"
 	"io"
 
 	"github.com/chettriyuvraj/leveldb-clone/common"
 	"github.com/chettriyuvraj/leveldb-clone/skiplist"
+	"github.com/chettriyuvraj/leveldb-clone/sstable"
 )
 
 const (
@@ -17,8 +17,7 @@ const (
 	DEFAULTINDEXDISTANCE = 15
 )
 
-var ErrEmptyKeyNotAllowed = errors.New("no SSTable data to write")
-var ErrNoSSTableDataToWrite = errors.New("no SSTable data to write")
+var ErrEmptyKeyNotAllowed = errors.New("empty key not allowed")
 
 type MemDB struct {
 	skiplist.SkipList
@@ -30,16 +29,6 @@ type MemDBIterator struct {
 	curNode            *skiplist.Node
 	hasEnded           bool
 	err                error
-}
-
-type SSTableDirectory struct {
-	entries []*SSTableDirEntry
-}
-
-type SSTableDirEntry struct {
-	len    uint32
-	key    []byte
-	offset uint64
 }
 
 func (db *MemDB) String() string {
@@ -118,114 +107,6 @@ func (db *MemDB) Delete(key []byte) error {
 	return nil
 }
 
-func (db *MemDB) RangeScan(start, limit []byte) (common.Iterator, error) {
-	iter := NewMemDBIterator(db, start, limit)
-	return iter, iter.Error()
-}
-
-func (db *MemDB) FullScan() (common.Iterator, error) {
-	iter := NewMemDBIterator(db, db.FirstKey(), nil)
-	return iter, iter.Error()
-}
-
-func (db *MemDB) FlushSSTable(f io.Writer) error {
-	data, err := db.getSSTableData(DEFAULTINDEXDISTANCE)
-	if err != nil {
-		return fmt.Errorf("error flushing to SSTable: %w", err)
-	}
-
-	_, err = f.Write(data)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-/*
-SSTable notes:
--> Directory is the index to offset of each key in the table, so we can find keys without searching the whole table
-SSTableFormat:
-1. 0-7 bytes: offset to start of 'directory'
-2. [start of data] [key_length(4 bytes):key:val_length(4 bytes):val] x Number of keys
-3. [start of directory] [key_length(4 bytes):key:key_offset(8 bytes)] x Number of keys
-This looks like
-[DirectoryOffset]
-[Data]
-[Directory]
-*/
-
-func (db *MemDB) getSSTableData(distBetweenIndexKeys int) (data []byte, err error) {
-	iter, err := db.FullScan()
-	if err != nil {
-		return nil, fmt.Errorf("error getting SSTable: %w", err)
-	}
-
-	/* Scan all entries in sorted order + keep track of their offsets + construct SSTable */
-	dir := SSTableDirectory{}
-	curOffset, curDistanceBetweenKeys := 8, 0
-	for {
-		k, v := iter.Key(), iter.Value()
-		kvSize := len(k) + len(v)
-		if k == nil {
-			break
-		}
-
-		/* Only append entry to index if distance between keys ~ distBetween keys OR key is the first key, since we are creating a sparse index  */
-		if len(data) == 0 || curDistanceBetweenKeys+kvSize > distBetweenIndexKeys {
-			dirEntry := &SSTableDirEntry{key: k, offset: uint64(curOffset)}
-			dir.entries = append(dir.entries, dirEntry)
-			curDistanceBetweenKeys = 0
-		} else {
-			curDistanceBetweenKeys += kvSize
-		}
-
-		/* Append data record to data */
-		dataRecord := createSSTableDataRecord(k, v)
-		curOffset += len(dataRecord)
-		data = append(data, dataRecord...)
-
-		if nextExists := iter.Next(); !nextExists {
-			break
-		}
-	}
-
-	if len(data) == 0 {
-		return nil, ErrNoSSTableDataToWrite
-	}
-
-	/* Note the offset at which directory starts + construct the directory for SSTable  */
-	dirOffset := binary.BigEndian.AppendUint64([]byte{}, uint64(curOffset))
-	dirData := []byte{}
-	for _, entry := range dir.entries {
-		keyLen := binary.BigEndian.AppendUint32([]byte{}, uint32(len(entry.key)))
-		keyOffset := binary.BigEndian.AppendUint64([]byte{}, entry.offset)
-		dirData = append(dirData, keyLen...)
-		dirData = append(dirData, entry.key...)
-		dirData = append(dirData, keyOffset...)
-	}
-
-	/* Combine directoryOffset:SSTableData:directory*/
-	data = append(dirOffset, data...)
-	data = append(data, dirData...)
-
-	return data, nil
-
-}
-
-/*
-Format for a single record: [key_length(4 bytes):key:val_length(4 bytes):val]
-*/
-func createSSTableDataRecord(k, v []byte) (record []byte) {
-	record = binary.BigEndian.AppendUint32(record, uint32(len(k)))
-	record = append(record, k...)
-	record = binary.BigEndian.AppendUint32(record, uint32(len(v)))
-	if len(v) > 0 {
-		record = append(record, v...)
-	}
-	return record
-}
-
 /* Note: limitKey -> nil indicates scan till end of range */
 func NewMemDBIterator(db *MemDB, startKey, limitKey []byte) *MemDBIterator {
 	iter := MemDBIterator{MemDB: db, startKey: startKey, limitKey: limitKey}
@@ -243,6 +124,35 @@ func NewMemDBIterator(db *MemDB, startKey, limitKey []byte) *MemDBIterator {
 	}
 
 	return &iter
+}
+
+func (db *MemDB) RangeScan(start, limit []byte) (common.Iterator, error) {
+	iter := NewMemDBIterator(db, start, limit)
+	return iter, iter.Error()
+}
+
+func (db *MemDB) FullScan() (common.Iterator, error) {
+	iter := NewMemDBIterator(db, db.FirstKey(), nil)
+	return iter, iter.Error()
+}
+
+func (db *MemDB) FlushSSTable(f io.Writer) error {
+	iter, err := db.FullScan()
+	if err != nil {
+		return err
+	}
+
+	data, err := sstable.GetSSTableData(iter, DEFAULTINDEXDISTANCE)
+	if err != nil {
+		return fmt.Errorf("error flushing to SSTable: %w", err)
+	}
+
+	_, err = f.Write(data)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 /*
