47fbd72125d0eb65920df207998095df247f8b6f
Yuvraj Chettri <chettriyuvraj@gmail.com>
2024-04-13 00:09:34 +0530

    feat: add new error that indicates val doesn't exist

--- common/errors.go
+++ common/errors.go
@@ -2,6 +2,7 @@ package common
 
 import "errors"
 
-var ErrKeyDoesNotExist = errors.New("this key does not exist")
+var ErrKeyDoesNotExist = errors.New("key does not exist")
+var ErrValDoesNotExist = errors.New("val does not exist")
 var ErrIdxOutOfBounds = errors.New("index out of bounds")
 var ErrInvalidRange = errors.New("range is invalid")
