07f88b273ac85237c8a1fafec0ca9e28e5fc3150
Yuvraj Chettri <chettriyuvraj@gmail.com>
2024-04-16 01:32:12 +0530

    feat: error type for tombstone

--- common/errors.go
+++ common/errors.go
@@ -2,6 +2,7 @@ package common
 
 import "errors"
 
+var ErrTombstoneEncountered = errors.New("tombstone encountered")
 var ErrKeyDoesNotExist = errors.New("key does not exist")
 var ErrValDoesNotExist = errors.New("val does not exist")
 var ErrIdxOutOfBounds = errors.New("index out of bounds")
